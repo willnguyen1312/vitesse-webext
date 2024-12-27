af1595a41df783a1bd111bd8eddafb6f17c5cec9
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-12-27 13:12:08 -0700

    ^o^

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,11 +1,5 @@
 /* eslint-disable no-console */
 
-const listOfUrls = [
-  'https://wechoice.vn/chi-tiet-de-cu-6/quang-hung-masterd-512.htm',
-  'https://wechoice.vn/chi-tiet-de-cu-1/ca-si-quang-hung-masterd-678.htm',
-  'https://wechoice.vn/chi-tiet-de-cu-18/muzik-fc-quang-hung-masterd-698.htm',
-]
-
 async function vote() {
   let button = document.querySelector('.vote-action.js-vote-action') as HTMLButtonElement
 
@@ -13,6 +7,8 @@ async function vote() {
     await new Promise(resolve => setTimeout(resolve, 3000))
     button = document.querySelector('.vote-action.js-vote-action') as HTMLButtonElement
   }
+
+  await new Promise(resolve => setTimeout(resolve, 3000))
 
   button.scrollIntoView({
     behavior: 'smooth',
@@ -22,7 +18,7 @@ async function vote() {
   button.click()
 }
 
-const oneMinute = 10 * 1000;
+const timer = 30 * 1000;
 
 (async () => {
   const isWeChoice = window.location.href.includes('wechoice.vn')
@@ -30,16 +26,10 @@ const oneMinute = 10 * 1000;
     console.log('This is WeChoice website')
   }
 
-  const index = listOfUrls.findIndex(url => window.location.href === url)
-  if (index === -1) {
-    window.location.replace(listOfUrls[0])
-    return
-  }
-
   await vote()
 
   // Wait for 30 seconds
-  await new Promise(resolve => setTimeout(resolve, oneMinute))
-  const nextIndex = (index + 1) % listOfUrls.length
-  window.location.replace(listOfUrls[nextIndex])
+  setTimeout(() => {
+    window.location.reload()
+  }, timer)
 })()
